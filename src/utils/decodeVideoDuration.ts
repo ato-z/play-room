3f0d8b4bc0e6a4057aca1098553400f9f9e63e25
atoz <atoz@atozdeMac-mini.local>
2022-03-05 16:05:40 +0800

    计算在线视频时长，依赖 ffmpeg

--- src/utils/decodeVideoDuration.ts
+++ src/utils/decodeVideoDuration.ts
@@ -1,8 +1,46 @@
+import {exec} from 'child_process'
+/**
+ * 根据时间字符串计算出对应的毫秒
+ * @param date {string} 时间字符串 00:22:52.10
+ * @returns 相加的毫秒数
+ * ```
+ * decodeDateToTime('00:22:52.10') // => 1372010
+ * ```
+ */
+const decodeDateToTime = (date: string) => {
+    const [times, ms] = date.split('.')
+    const vals = times.split(':')
+    let val = parseInt(ms) || 0
+    let current
+    let deep = 0
+    while(current = vals.pop()) {
+        const pow = deep === 0 ? 1 : Math.pow(60, deep)
+        const currentVal = parseInt(current) * pow
+        val += currentVal * 1000
+        deep += 1
+    }
+    return val
+}
+
 /**
  * 计算线上视频时长的助手函数后期实现
+ * 服务端需先安装 ffmpeg
  * @param url 
  * @returns 
  */
 export const decodeVideoDuration = async (url: string) => {
-    return 25 * 60 * 1000
+    return new Promise((resovle, reject) => {
+        exec(`ffmpeg -i ${url}`, function(err, stdout, stderr){
+            if (err) { reject(err) }
+            var reslut = stderr.toString();
+            const regDuration =/Duration\: ([0-9\:\.]+),/;
+            const duration = regDuration.exec(reslut); 
+            if(duration !== null){
+                var dateStr = duration[1];
+                //获得时长
+                resovle(decodeDateToTime(dateStr))
+            }
+            reject(new Error('解析失败'))
+        })
+    }).catch(() => 120 * 60 * 1000)
 }
